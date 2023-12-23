5821804896b0f5928cb72821448078be3d7cef22
Gordon Reynolds <thisisgordonsemail@gmail.com>
2023-12-23 14:20:34 -0800

    Swapped to a class, added secrets file

---
+++ cfn_secrets_example.py
@@ -0,0 +1,18 @@
+"""Secrets file for CFN Stats Scraper. Replace these with values from your own login
+   Rename file to 'cfn_secrets.py'
+"""
+
+# URL_TOKEN & PLAYER_ID:
+# Go to profile and click "stats" to get the following URL (use Fiddler):
+# https://www.streetfighter.com/6/buckler/_next/data/{URL_TOKEN_HERE}/en/profile/{PLAYER_ID_HERE}/play.json?sid={PLAYER_ID_HERE}
+
+URL_TOKEN:str = 'AAABBBCCCDDD'
+DEFAULT_PLAYER_ID:str = '3425126856' # Scrub's CFN
+
+# BUCKLER_ID & BUCKLER_R_ID & PRAISE_DATE
+# You get this from the cookie the website stores. (use Fiddler)
+# Look for the "buckler_id" and "buckler_praise_date" values.
+
+BUCKLER_ID:str = 'okH4OHGYRUDiMTIzLWrchv2YjwESkOusG3HsOtAHw2CMXkZJFGcHPz4BEYssy1QR'
+BUCKLER_R_ID:str = ''
+BUCKLER_PRAISE_DATE:str = '1703310838092'
--- scrape.py
+++ scrape.py
@@ -1,55 +1,66 @@
 """Module providing for the scraping of CFN for player stats"""
 
 import requests
+import cfn_secrets
 
-# user id values
-url_token:str = '7R1j0S8w9k0X3T3Db5_0h'
-buckler_id:str = 'okH4OHGYRUDiMTIzLWrchv2YjwESkOusG3HsOtAHw2CMXkZJFGcHPz4BEYssy1QR'
-buckler_r_id:str = ''
-buckler_praise_date:str = '1703310838092'
+class CFNStatsScraper:
+    """Object that grabs the data from the website"""
 
-player_id:str = '3425126856' #scrub
+    _HOSTNAME:str = 'www.streetfighter.com'
+    _CFN_ROOT:str = f'https://{_HOSTNAME}/6/buckler/'
+    _PROFILE_URL:str = _CFN_ROOT + 'en/profile/{player_id}'
+    _PLAYER_STATS_URL:str = _CFN_ROOT + '/_next/data/{url_token}/en'\
+        '/profile/{player_id}.json?sid={player_id}'
 
-cookies:dict[str, str] = {
-    'buckler_id' : buckler_id, 
-    'buckler_r_id' : buckler_r_id, 
-    'buckler_praise_date': buckler_praise_date
-}
+    _cookies:dict[str, str] = {
+        'buckler_id' : cfn_secrets.BUCKLER_ID,
+        'buckler_r_id' : cfn_secrets.BUCKLER_R_ID,
+        'buckler_praise_date': cfn_secrets.BUCKLER_PRAISE_DATE
+    }
 
-headers:dict[str, str] = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en,q=0.9',
-    'Cache-Content': 'no-cache',
-    'Connection': 'keep-alive',
-    'Host': 'www.streetfighter.com',
-    'Pragma': 'no-cache',
-    'Referer': 'https://www.streetfighter.com/6/buckler/profile/4249556471/play',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0'
-}
+    _headers:dict[str, str] = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en,q=0.9',
+        'Cache-Content': 'no-cache',
+        'Connection': 'keep-alive',
+        'Host': 'www.streetfighter.com',
+        'Pragma': 'no-cache',
+        'Referer': 'https://www.streetfighter.com/6/buckler/profile/4249556471/play',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0)'\
+                'Gecko/20100101 Firefox/120.0'
+    }
 
-url:str = f'https://www.streetfighter.com/6/buckler/_next/data/{url_token}/en/profile/{player_id}.json?sid={player_id}'
+    def __init__(self, **kwargs):
+        self.player_id:str = cfn_secrets.DEFAULT_PLAYER_ID \
+            if 'player_id' not in kwargs \
+                else kwargs['player_id']
 
-response = requests.get(
-    url,
-    timeout=5,
-    headers=headers,
-    cookies=cookies
-    )
+    def get_player_stats(self) -> None:
+        """Grabs the basic profile data for the user."""
 
-profile_json_data = response.json()
+        response = requests.get(
+            self._PLAYER_STATS_URL.format(
+                url_token=cfn_secrets.URL_TOKEN,
+                player_id=self.player_id),
+            timeout=5,
+            headers=self._headers,
+            cookies=self._cookies
+            )
 
-player_character_winrates = profile_json_data['pageProps']['play']['character_win_rates']
+        profile_json_data = response.json()
 
-for player in player_character_winrates:
-    if player['character_alpha'] == 'DHALSIM':
-        stats = player
+        player_character_winrates = profile_json_data['pageProps']['play']['character_win_rates']
 
-print(f"Scrub has used {stats['character_name']} {stats['battle_count']} times this phase.")
+        for player in player_character_winrates:
+            if player['character_alpha'] == 'DHALSIM':
+                stats = player
 
+        print(f"Scrub has used {stats['character_name']} {stats['battle_count']} times this phase.")
 
-# TODO
-# https://github.com/Xjph/CFNScrape/tree/master/CFNScrape
+if __name__ == '__main__':
+    cfn_scraper = CFNStatsScraper()
+    cfn_scraper.get_player_stats()
