f3e0669d00aa0c8c22f8896d2e3c2a278ad885d4
Gordon Reynolds <thisisgordonsemail@gmail.com>
2023-12-23 18:25:44 -0800

    Swapping os.path for pathlib

--- scrape.py
+++ scrape.py
@@ -1,8 +1,9 @@
 """Module providing for the scraping of CFN for player stats"""
 
 import cfn_secrets
+import datetime
 import json
-import os.path
+import pathlib
 import requests
 import sys
 
@@ -12,7 +13,7 @@ class CFNStatsScraper:
     _HOSTNAME:str = 'www.streetfighter.com'
     _CFN_ROOT:str = f'https://{_HOSTNAME}/6/buckler/'
     _PROFILE_URL:str = _CFN_ROOT + 'en/profile/{player_id}'
-    _PLAYER_STATS_URL:str = _CFN_ROOT + '/_next/data/{url_token}/en'\
+    _PLAYER_PROFILE_URL:str = _CFN_ROOT + '/_next/data/{url_token}/en'\
         '/profile/{player_id}.json?sid={player_id}'
 
     _cookies:dict[str, str] = {
@@ -43,17 +44,26 @@ class CFNStatsScraper:
             if 'player_id' not in kwargs \
                 else kwargs['player_id']
 
-    def _fetch_player_stats(self) -> dict:
+    @property
+    def _profile_cache_dir(self) -> pathlib.Path:
+        """Generate the filename and path for the player's profile json cache."""
+        
+        dt = datetime.datetime.now()
+
+        # $/player_stats/YYYY/MM/DD/{player_id}/{player_id}_profile.json
+        return pathlib.Path(f'player_stats/{str(dt.year)}/{str(dt.month)}/{str(dt.day)}/{self.player_id}')
+
+    def _fetch_player_profile_json(self) -> dict:
         """Grabs the basic profile data for the user."""
 
-        if self._check_if_stats_exist():
+        if self._cache_exist:
             print("Cached data found.")
             return self._load_cached_data()
 
         print("Making request for new data!")
 
         response: requests.Response = requests.get(
-            self._PLAYER_STATS_URL.format(
+            self._PLAYER_PROFILE_URL.format(
                 url_token=cfn_secrets.URL_TOKEN,
                 player_id=self.player_id),
             timeout=5,
@@ -68,56 +78,69 @@ class CFNStatsScraper:
         profile_json_data:dict = response.json()
 
         # Store it for cache purposes
-        self._store_player_stats(profile_json_data)
+        self._store_player_profile(profile_json_data)
 
         return profile_json_data
 
-    def _check_if_stats_exist(self) -> bool:
+    @property
+    def _cache_exist(self) -> bool:
         """Returns true if the requested player's stats for this date exists."""
 
-        if not os.path.exists('player_stats'):
-            print("Player stats directory 'player_stats' missing!")
-            return False
+        cache_path = self._profile_cache_dir
 
-        if not os.path.isfile(os.path.join('player_stats', 'player_data.json')):
+        if not pathlib.Path.exists(cache_path):
+            pathlib.Path.mkdir(cache_path, parents=True, exist_ok=True)
+            print(f"Player stats directory {cache_path} missing, creating.")
+
+        file_path = cache_path / f'{self.player_id}_profile.json'
+        if not pathlib.Path.is_file(file_path):
             print("Missing player_data.json file!")
             return False
 
         return True
 
-    def _store_player_stats(self, player_stats:dict):
+
+    def _store_player_profile(self, player_stats:dict):
         """Store the json object with logical names."""
 
-        with open(os.path.join('player_stats', 'player_data.json'), 'w', encoding='utf-8') as f:
+        with open(pathlib.Path(self._profile_cache_dir, f'{self.player_id}_profile.json'), 'w', encoding='utf-8') as f:
             json.dump(player_stats, f, ensure_ascii=False, indent=4)
 
         print("Stored player data.")
 
 
-
     def _load_cached_data(self) -> dict:
         """Fetch the already scraped json data."""
 
-        with open(os.path.join('player_stats', 'player_data.json'), encoding='utf-8') as f:
+        with open(pathlib.Path(self._profile_cache_dir, f'{self.player_id}_profile.json'), 'r', encoding='utf-8') as f:
             return json.loads(f.read())
 
 
-    def get_player_stats(self) -> None:
-        """Display some stats to see if the code is working."""
+    def get_player_profile(self) -> None:
+        """Display some stuff to see if the code is working."""
 
-        player_stats:dict = self._fetch_player_stats()
+        player_profile_json:dict = self._fetch_player_profile_json()
+
+        player_name:str = player_profile_json['pageProps']['fighter_banner_info']['personal_info']['fighter_id']
 
         player_character_winrates:dict = \
-            player_stats['pageProps']['play']['character_win_rates']
+            player_profile_json['pageProps']['play']['character_win_rates']
 
-        stats:dict = {}
+        player_profile:dict = {}
+
+        chars = ['DHALSIM', 'ZANGIEF', 'DEE JAY']
 
         for player in player_character_winrates:
-            if player['character_alpha'] == 'DHALSIM':
-                stats = player
-
-        print(f"Scrub has used {stats['character_name']} {stats['battle_count']} times this phase.")
+            if player['character_alpha'] in chars:
+                player_profile = player
+                print(f"{player_name} has used {player_profile['character_name']} {player_profile['battle_count']} times this phase.")
 
 if __name__ == '__main__':
-    cfn_scraper = CFNStatsScraper()
-    cfn_scraper.get_player_stats()
+    args = sys.argv[1:]
+    if len(args) == 1:
+        player_id = args[0]
+    else:
+        player_id = cfn_secrets.DEFAULT_PLAYER_ID
+
+    cfn_scraper = CFNStatsScraper(player_id = player_id)
+    cfn_scraper.get_player_profile()
