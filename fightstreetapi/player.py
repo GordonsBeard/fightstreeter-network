443c48619ea93e7d956419a668e9226cc809f916
Gordon Reynolds <thisisgordonsemail@gmail.com>
2025-04-19 11:42:22 -0700

    Allowing for player stats call to be range or singular

--- fightstreetapi/player.py
+++ fightstreetapi/player.py
@@ -3,7 +3,7 @@
 from dataclasses import field
 
 from apiflask import APIBlueprint, abort
-from apiflask.validators import OneOf
+from apiflask.validators import Length, OneOf
 from marshmallow_dataclass import dataclass
 
 from constants import charid_map
@@ -49,18 +49,32 @@ class PlayerHistoricStats:  # pylint: disable=too-many-instance-attributes
 class DateRangeRequest:
     """Historic stats for a player between two given dates"""
 
+    player_id: str = field(
+        metadata={
+            "required": True,
+            "validate": Length(equal=10),
+            "metadata": {"example": "3425126856"},
+        }
+    )
     date_start: str = field(
         metadata={
             "required": True,
-            "validate": OneOf(db.dates_with_data()),
             "metadata": {"example": "2025-04-01"},
         }
     )
+
     date_end: str = field(
         metadata={
             "required": True,
-            "validate": OneOf(db.dates_with_data()),
             "metadata": {"example": "2025-04-17"},
+        }
+    )
+
+    fetch_range: bool = field(
+        metadata={
+            "required": False,
+            "validate": OneOf([True, False]),
+            "metadata": {"example": True},
         }
     )
 
@@ -71,30 +85,41 @@ class CharacterRanking:
 
     date: str
     char_id: str
-    lp: str
-    mr: str
+    lp: int
+    mr: int
 
 
-@bp.post("/overview/<string:player_id>")
-@bp.input(DateRangeRequest.Schema, location="json", arg_name="dates")  # type: ignore # pylint: disable=maybe-no-member
+@bp.post("/overview")
+@bp.input(DateRangeRequest.Schema, location="query")  # type: ignore # pylint: disable=maybe-no-member
 @bp.output(PlayerHistoricStats.Schema(many=True))  # type: ignore # pylint: disable=maybe-no-member
-def player_historicstats(player_id, dates) -> list[PlayerHistoricStats]:
-    """Player's historic stats between two dates"""
+@bp.doc(
+    summary="Player's overview snapshot",
+    description="Returns the historic_stats row containing the summary of stats for the player for each given date.",
+)
+def player_overview_snapshot(query_data) -> list[PlayerHistoricStats]:
+    """Player's historic_stats snapshot between two dates."""
+
+    range_sql = (
+        """(hs.date BETWEEN ? AND ?)"""
+        if query_data.fetch_range
+        else """(hs.date = ? OR hs.date = ?)"""
+    )
 
     # Support username or id, start with username
     historic_stats_daterange_sql: str = (
-        """SELECT date, hs.player_name, hs.player_id, joined_at date_joined, selected_char as selected_character,
+        f"""SELECT date, hs.player_name, hs.player_id, joined_at date_joined, selected_char as selected_character,
                 lp as selected_lp, mr as selected_mr, hub_matches, ranked_matches, casual_matches,
                 custom_matches, hub_time, ranked_time, casual_time, custom_time, extreme_time,
                 versus_time, practice_time, arcade_time, wt_time, total_kudos, thumbs, last_played,
                 profile_tagline, title_text, title_plate
             FROM historic_stats hs
             INNER JOIN club_members cm ON cm.player_id = hs.player_id
-            WHERE hs.date BETWEEN ? AND ? AND hs.player_id = ?"""
+            WHERE hs.player_id = ? AND {range_sql}"""
     )
 
     results = db.query_db(
-        historic_stats_daterange_sql, (dates.date_start, dates.date_end, player_id)
+        historic_stats_daterange_sql,
+        (query_data.player_id, query_data.date_start, query_data.date_end),
     )
 
     if not results:
@@ -109,30 +134,41 @@ def player_historicstats(player_id, dates) -> list[PlayerHistoricStats]:
     return player_overview
 
 
-@bp.post("/ranking/<string:player_id>")
-@bp.input(DateRangeRequest.Schema, location="json", arg_name="dates")  # type: ignore # pylint: disable=maybe-no-member
+@bp.post("/ranking")
+@bp.input(DateRangeRequest.Schema, location="query")  # type: ignore # pylint: disable=maybe-no-member
 @bp.output(CharacterRanking.Schema(many=True))  # type: ignore # pylint: disable=maybe-no-member
 @bp.doc(
-    summary="Player's characters' stats",
-    description="Returns the LP/MR for every character played by a given user.",
+    summary="Player's ranking snapshot",
+    description="Returns the LP/MR for every character played by a given user for the given date.",
 )
-def ranking_stats_range(player_id, dates) -> list[CharacterRanking]:
+def player_ranking_snapshot(query_data) -> list[CharacterRanking]:
     """Stats on every character played by a given user"""
 
-    # Support username or id, start with username
     player_characters_sql: str = (
-        """SELECT date, char_id, lp, mr
+        (
+            """SELECT date, char_id, lp, mr
             FROM ranking
             WHERE date BETWEEN ? AND ? AND player_id = ?;"""
+        )
+        if query_data.fetch_range
+        else (
+            """SELECT date, char_id, lp, mr
+            FROM ranking
+            WHERE (date = ? OR date = ?) AND player_id = ?;"""
+        )
     )
 
     result = db.query_db(
-        player_characters_sql, (dates.date_start, dates.date_end, player_id)
+        player_characters_sql,
+        (query_data.date_start, query_data.date_end, query_data.player_id),
     )
 
     if not result:
         abort(404)
 
     list_of_char_ranks = [CharacterRanking(**row) for row in result]
+
+    for character_rank in list_of_char_ranks:
+        character_rank.char_id = charid_map[character_rank.char_id]
 
     return list_of_char_ranks
